42274e55f86fa34d1f89b3ac81129ab3670c554f
Oualid <bouterbiatoualid@gmail.com>
2020-09-22 20:17:06 +0200

    [evo] setup navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
 
 function App() {
   return (
-      <Router>
-        <div className="app">
-         <Switch>
-             <Route path="/login">
-                 <h1>Login</h1>
-             </Route>
-             <Route path="/checkout">
-                 <h1>Checkout</h1>
-             </Route>
-             <Route path="/">
-                 <h1>Home</h1>
-             </Route>
-         </Switch>
-        </div>
-      </Router>
-
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+const IMAGE_URL =
+  "https://images-eu.ssl-images-amazon.com/images/G/08/digital/video/gateway/placement/launch/BrutusVsCesar/BRUCE_2020_GWBleedingHero_COVIDUPDATE_ENG_XSite_1500x600_PV_fr-FR._CB403749904_.jpg";
+function Home() {
+  return (
+    <div className="home">
+      <img classNAme="home__image" src={IMAGE_URL} alt="" />
+    </div>
+  );
+}
+
+export default Home;
